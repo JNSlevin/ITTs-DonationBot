d12a603e06ede9aeae7949b4b211e9dedf8b2abc
Sharlikran <danielhmpdx16@gmail.com>
2022-06-29 08:29:29 -0700

    added remaining files

---
+++ i18n/de.lua
@@ -0,0 +1,63 @@
+ITTsDonationBot.i18n = {
+
+	ITTDB_NAME 					= "ITT's Donation Bot",
+
+    ITTDB_HEADER 				= "Spenden",
+    ITTDB_HEADER_GUILDS         = "Gilden",
+    ITTDB_HEADER_NOTIFY         = "Benachrichtigungen",
+
+	-- time
+	ITTDB_TIME_TODAY 			= "Heute",
+	ITTDB_TIME_YESTERDAY		= "Gestern",
+	ITTDB_TIME_2_DAYS 			= "Vorgestern",
+	ITTDB_TIME_THIS_WEEK 		= "Diese Woche",
+	ITTDB_TIME_LAST_WEEK 		= "Letzte Woche",
+	ITTDB_TIME_PRIOR_WEEK 		= "Vorletzte Woche",
+	ITTDB_TIME_7_DAYS 			= "Letzte 7 Tage",
+	ITTDB_TIME_10_DAYS 			= "Letzte 10 Tage",
+	ITTDB_TIME_14_DAYS 			= "Letzte 14 Tage",
+	ITTDB_TIME_30_DAYS 			= "Letzte 30 Tage",
+	ITTDB_TIME_TOTAL 			= "Gesamt",
+
+	-- settings
+	ITTDB_SETTINGS_DESC 			= [[Wenn Ihr die korrekten Berechtigungen in einer Gilde habt, wird die Option die Gilde zu scannen freigegeben. Wenn eine Gilde deaktiviert ist, habt Ihr nicht die "Gold in der Gildenbank ansehen" Berechtigung!]],
+  	
+  	ITTDB_SETTINGS_SCAN_ERROR		= "Ihr habt nicht die Berechtigung, um diese Gilde zu scannen",
+  	ITTDB_SETTINGS_SCAN_PROMPT 		= "Aktiviere oder deaktiviere, das scannen von ${guild}",
+  	ITTDB_SETTINGS_SCAN_INFO 		= [[Wenn die obige Gildenliste aufgrund einer kürzlichen Änderung nicht korrekt ist, muss möglicherweise der Befehl /reloadui ausgeführt werden, es ist leider nicht immer möglich Dinge zu automatiseren.]],
+	
+  	ITTDB_SETTINGS_NOTIFY 			= [[Benachrichtigungen werden nur für Dinge in den letzten 24 Stunden erfolgen.]],
+  	ITTDB_SETTINGS_CHAT 			= "Chat-Benachrichtigungen",
+  	ITTDB_SETTINGS_SCREEN			= "Bildschirm-Benachrichtigungen",
+
+	ITTDB_SETTINGS_LOTTO_NAME 		= "Lotterie-Listen Generator",
+	ITTDB_SETTINGS_LOTTO_DESC 		= [[Klicken um die Spalten der Mitgliedernamen und derer Ticketanzahl für eure Lotterietabelle zu generieren. Ihr könnt die Werte für die Tickets weiter unten verändern, Standartmäßig ist 1.000 Gold = 1 Ticket ausgewählt.]],
+
+	ITTDB_SETTINGS_LOTTO_SELECT 		= "Ausgewählte Gilde",
+	ITTDB_SETTINGS_LOTTO_GUILD 			= "Gildenmitglieder für die Lotterie",
+	ITTDB_SETTINGS_LOTTO_VALUE 			= "Lotterieticket Wert",
+	ITTDB_SETTINGS_LOTTO_EXAMPLE 		= "Beispiel: 1.000 Gold pro Ticket",
+	ITTDB_SETTINGS_LOTTO_TIMEFRAME 		= "Ausgewähltes Zeitfenster",
+	ITTDB_SETTINGS_LOTTO_TIMEFRAME_2 	= "Zeitfenster für Lotterie",
+	ITTDB_SETTINGS_LOTTO_GENERATE 		= "Generieren",
+	ITTDB_SETTINGS_LOTTO_GENERATE_COL 	= "Klicken um die Tabellenspalten zu generieren",
+	ITTDB_SETTINGS_LOTTO_INFO_1		 	= "Werte für die Namensspalte, Zeile 1-250",
+	ITTDB_SETTINGS_LOTTO_INFO_2		 	= "Werte für die Ticketspalte, Zeile 1-250",
+	ITTDB_SETTINGS_LOTTO_INFO_3		 	= "Werte für die Namensspalte, Zeile 251-500",
+	ITTDB_SETTINGS_LOTTO_INFO_4		 	= "Werte für die Ticketspalte, Zeile 251-500",
+	ITTDB_SETTINGS_LOTTO_NAMES		 	= "Namenswerte",
+	ITTDB_SETTINGS_LOTTO_TICKETS		= "Ticketwerte",
+	ITTDB_SETTINGS_LOTTO_INFO_5 		= "Namensspalte, wird hier generiert",
+	ITTDB_SETTINGS_LOTTO_INFO_6 		= "Ticketspalte, wird hier generiert",
+
+	-- Notifications
+	ITTDB_NOTIFICATION = "${time}: ${user} hat ${amount} an ${guild} gespendet",
+
+	-- Null
+	ITTDB_NONE 					= "Keine",
+
+	-- commands
+	ITTDB_CMD_NO_GUILDS 		= "Konnte keine Gilde finden, bitte füge eine Nummer zwischen 1 und 5 hinzu.",
+	ITTDB_CMD_GENERATED 		= "Kurzinfos neu generiert",
+
+}
